39d1a248fbad16e07d28d6f9307537e9e85728c3
DylanRees07 <Dylanrees07@gmail.com>
2022-10-28 21:44:57 +0200

    Added and styled end game model

--- Specno/Memory Game/main.js
+++ Specno/Memory Game/main.js
@@ -2,17 +2,20 @@
 //Store players and start game from model
 player1 = {
     player: "",
-    score: 0
+    score: 0,
+    icon: "Assets/Astronaut1.png"
 }
 
 player2 = {
     player: "",
-    score: 0
+    score: 0,
+    icon: "Assets/Astronaut2.png"
 }
 
+matches = 0
 
 $('#start-game').click(function(){
-        $('.model-container').toggleClass('hidden')
+        $('.start-model-container').toggleClass('hidden')
         player1.player = $('#inpplayer1name').val()
         player2.player = $('#inpplayer2name').val()
         $('#player1name').text(player1.player)
@@ -53,6 +56,31 @@ for (var i = 0; i < cards.length; i++){
     cards.eq(i).css({'background-image':`url(\'Assets/${cards.eq(i).attr('id')}.svg\')`})
 }
 
+//End game and show winner
+endgame = function(){
+    var winner
+    if (player1.score > player2.score){
+        winner = player1
+        loser = player2
+    }
+    else{ 
+        winner = player2
+        loser = player1
+    }
+    $('.main-content').toggleClass("hidden")
+    $('.end-model-container').toggleClass('hidden')
+    $('#h1winner').text(winner.player)
+    $('#div-winner img').attr('src',winner.icon)
+    $('#winner-name').text(winner.player)
+    $('#winner-score').text(`Score: ${winner.score}`)
+    $('#div-loser img').attr('src',loser.icon)
+    $('#loser-name').text(loser.player)
+    $('#loser-score').text(`Score: ${loser.score}`)
+    $('#play-again').click(function(){
+        location.reload()
+    })
+}
+
 //Check if match
 function checkMatch(){
     high = Math.max(flipped[0],flipped[1])
@@ -64,6 +92,7 @@ function checkMatch(){
             currentplayer.score += 1
             highcard.toggleClass('hidden')
             lowcard.toggleClass('hidden')
+            matches += 1;
         }
         highcard.toggleClass('down')
         lowcard.toggleClass('down')
@@ -82,9 +111,11 @@ function checkMatch(){
             default:
                 break;
         }
+        if (matches === 1){
+            endgame()
+        }
     }
     , 1500);
-
 
 }
 
